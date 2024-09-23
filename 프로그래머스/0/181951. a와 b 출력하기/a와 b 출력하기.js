9a72f0c2dc65661919e3c6d94f4bdbdfd48eccaa
Woojoung Yoon <34205465+woojoung1217@users.noreply.github.com>
2024-09-23 16:29:55 +0900

    [level 0] Title: a와 b 출력하기, Time: 77.94 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -0,0 +1,10 @@
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', function (line) {
+    const [a, b] = line.split(' ')
+    console.log('a =', a)
+    console.log('b =', b)
+})
