2a87783e382feaffde28b310150f012b8ac939d5
Woojoung Yoon <34205465+woojoung1217@users.noreply.github.com>
2024-09-24 16:25:15 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    let a = Math.sqrt(n)
+    if(Number.isInteger(a) === true) {
+        return (a+1) ** 2
+    } else if (Number.isInteger(a) === false) {
+        return -1
+        
+    }
+    
+}
+
+// x = n / 2  2a = x 
