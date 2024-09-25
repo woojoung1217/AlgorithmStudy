1252cedcf508b8ef2b86877007197e0204bdeac4
Woojoung Yoon <34205465+woojoung1217@users.noreply.github.com>
2024-09-25 22:37:04 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,10 @@
+function solution(phone_number) {
+    var answer =''
+  
+    for(let i=0; i<phone_number.length-4; i++){
+        answer += "*"
+    }
+     answer += phone_number.slice(phone_number.length - 4);
+    
+    return answer
+}
