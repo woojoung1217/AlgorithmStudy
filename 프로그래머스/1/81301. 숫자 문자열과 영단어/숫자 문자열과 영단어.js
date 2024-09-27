1fce778de44d8c3e44f9e7e076364ee15781b515
Woojoung Yoon <34205465+woojoung1217@users.noreply.github.com>
2024-09-27 14:08:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 4.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    var answer =s;
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for(let i=0; i<numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i)
+        console.log(answer)
+    }
+    return Number(answer)
+}
