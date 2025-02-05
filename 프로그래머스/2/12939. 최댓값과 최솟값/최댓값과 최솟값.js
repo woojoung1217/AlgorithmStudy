a27c155392eed7e5943056ede4efc854618b0a42
Woojoung Yoon <34205465+woojoung1217@users.noreply.github.com>
2025-02-05 17:41:17 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,8 @@
 function solution(s) {
-    let numbers = s.split(' ').map(Number); // 문자열을 숫자 배열로 변환
-    let min = Math.min(...numbers); // 최소값 찾기
-    let max = Math.max(...numbers); // 최대값 찾기
-    return `${min} ${max}`; // "(최소값) (최대값)" 형식으로 반환
+   let arr = s.split(" ").map(v=>parseInt(v))
+   let max = Math.max(...arr); 
+   let min = Math.min(...arr);
+   let res = [min, max];
+   
+   return res.join(" ") 
 }
